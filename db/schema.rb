0340dc12ee705c8600728334eb45f1e04eda89e0
pjgr <pjgrooei@msn.com>
2023-12-04 23:49:55 +0900

    refactor:article_test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_25_104308) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_141945) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -29,11 +29,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_25_104308) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  create_table "gates", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_25_104308) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
   test "should be valid" do
-    article = Article.new(title: "Example Title", content: "Example Content")
+    article = Article.new(title: "Example Title", body: "Example Content", status: "public")
     assert article.valid?
   end
 end

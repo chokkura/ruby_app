1622384e75401cbed809ae6ecfdd9f02f8f4169e
pjgr <pjgrooei@msn.com>
2023-12-09 22:44:31 +0900

    yarn cache clean

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module RubyApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.nodejs.runtime = 'node'
   end
 end

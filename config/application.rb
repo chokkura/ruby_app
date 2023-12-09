9e857c6c1150accbccc6e9db310afefc264c5230
pjgr <pjgrooei@msn.com>
2023-12-09 22:48:26 +0900

    fix:config

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module RubyApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.nodejs.runtime = 'node'
+    config.javascript_runtime = :node
   end
 end

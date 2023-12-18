e31a8ffbbea4dc0ea171a6f0ca9f5a2b81dd68a9
pjgr <pjgrooei@msn.com>
2023-12-18 23:42:02 +0900

    try:elastic_search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,24 @@
 class Article < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   include Visible
 
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10}
+
+  # Elasticsearchのマッピングを設定
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english', index_options: 'offsets'
+      indexes :content, analyzer: 'english'
+    end
+  end
+
+  def as_indexed_json(options={})
+    self.as_json(
+      only: [:title, :content]
+    )
+  end
 end
